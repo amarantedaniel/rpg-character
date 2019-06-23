8bb7fc3ffe7808bc430338b54a5d515bde7ceb6d
Daniel Amarante <daniel.amarante2@gmail.com>
2019-06-23 13:08:01 -0300

    Initial Commit

---
+++ rpg-character/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  rpg-character
+//
+//  Created by Daniel Amarante on 6/23/19.
+//  Copyright © 2019 Tigerhood. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
